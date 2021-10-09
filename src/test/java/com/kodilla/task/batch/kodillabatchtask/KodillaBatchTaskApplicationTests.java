b6f31439fadb1e131252ea34ce2ecbbb499b75ad
Kacper Szubra <kacper.szubra@nvt.pl>
2021-10-10 00:12:51 +0200

    first commit

---
+++ src/test/java/com/kodilla/task/batch/kodillabatchtask/KodillaBatchTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.task.batch.kodillabatchtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaBatchTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
